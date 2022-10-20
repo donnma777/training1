737d99d2f48b5f0ecf2c902985d37d85707a0338
donnma777 <96525816+donnma777@users.noreply.github.com>
2022-10-21 02:42:49 +0900

    変更

--- src/page/FormKeys.js
+++ src/page/FormKeys.js
@@ -2,8 +2,6 @@
 const FormKeys = {
     formUrl: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfqt6xqvxNC-jAINoLQBbPPCt5ScyELggVvt8dfDkKGijsxwQ/formResponse",
     name: "entry.138770988",
-    // gender: "entry.1294529934",
-    // language: "entry.1993522736",
     mail: "entry.937318511",
     message: "entry.2079511436",
     corsAnywhere: ""
